08f422210100e2886b8c447cda48d948e80d9041
gaurav-8877 <156067605+gaurav-8877@users.noreply.github.com>
2025-11-20 00:52:25 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://real-time-chat-application-4-hcpi.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://real-time-chat-application-4-hcpi.onrender.com/" : "/api",
   withCredentials: true,
 });
